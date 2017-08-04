a1065892d748058210fbf311afbee488d105706e
jhoner <jhoner@hotmail.com>
2017-08-04 09:26:46 -0500

    Mi primer commin

---
+++ crear.php
@@ -0,0 +1,27 @@
+    <!-- Content Header (Page header) -->
+    <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <h1>
+        Registrar rol
+      </h1>
+      <ol class="breadcrumb">
+        <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+        <li class="active">Dashboard</li>
+      </ol>
+    </section>
+
+    <!-- Main content -->
+    <section class="content">
+  <form name="crearRol" method="POST" action="<?php echo getUrl("usuario","rol","postCrear")?>">
+    <div class="form-group">
+        <div class="col-md-7">
+          <input type="text" class="form-control" id="inputEmail3" placeholder="NOMBRE" name="rol_nombre">
+        </div>
+    </div>
+    <div class="form-group">
+      <div class="col-md-7">
+        <input type="submit" class="btn btn-info" value="Registrar">
+      </div>
+    </div>
+  </form>
